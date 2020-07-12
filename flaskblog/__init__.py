a72e8728be3121270009e4ea1aab968b204af1e6
Yogesh Sehgal <yogeshsehgal99@rediff.com>
2020-07-13 04:21:26 +0530

    first commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '69482c916d1d26fff30a67d1d97f37f9d6d23862'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
